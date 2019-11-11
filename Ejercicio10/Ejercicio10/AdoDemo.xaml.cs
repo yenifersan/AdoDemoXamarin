4c18ea47b5d7e99c693eaed593deb92c65db21c4
yenifersan <44785957+yenifersan@users.noreply.github.com>
2019-11-11 15:40:31 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio10/Ejercicio10/AdoDemo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AdoDemo : ContentPage
+    {
+        public AdoDemo()
+        {
+            InitializeComponent();
+            string output = string.Empty;
+            output = AdoExample.DoSomeDataAccess();
+            var layout = new StackLayout { Padding = new Thickness(5, 10) };
+            var label = new Label { Text = output, TextColor = Color.FromHex("#77d065"), FontSize = 20 };
+            layout.Children.Add(label);
+            this.Content = layout;
+        }
+    }
+}
+
---
+++ Ejercicio10/Ejercicio10/AdoExample.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+using Mono.Data.Sqlite;
+
+
+namespace Ejercicio10
+{
+    public static class AdoExample
+    {
+        public static SqliteConnection connection;
+        public static string DoSomeDataAccess()
+        {
+            string output = string.Empty;
+            // determine the path for the database file
+            string dbPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                "adodemo.db3");
+
+            bool exists = File.Exists(dbPath);
+
+            if (!exists)
+            {
+
+                // Need to create the database before seeding it with some data
+                Mono.Data.Sqlite.SqliteConnection.CreateFile(dbPath);
+                connection = new SqliteConnection("Data Source=" + dbPath);
+
+
+                // Open the database connection and create table with data
+                connection.Open();
+                CreateTable();
+            }
+            else
+            {
+
+                // Open connection to existing database file
+                connection = new SqliteConnection("Data Source=" + dbPath);
+
+                connection.Open();
+                var command = "DROP TABLE [Items];";
+                using (var c = connection.CreateCommand())
+                {
+                    c.CommandText = command;
+                    var rowcount = c.ExecuteNonQuery();
+                    Console.WriteLine("\tExecuted " + command);
+                }
+                CreateTable();
+
+            }
+
+            // query the database to prove data was inserted!
+            using (var contents = connection.CreateCommand())
+            {
+                contents.CommandText = "SELECT [_id], [Symbol] from [Items]";
+                var r = contents.ExecuteReader();
+
+                while (r.Read())
+                    output += String.Format("\n\tKey={0}; Value={1}",
+                                      r["_id"].ToString(),
+                                      r["Symbol"].ToString());
+
+            }
+
+            connection.Close();
+
+            return output;
+        }
+        private static void CreateTable()
+        {
+            var commands = new[] {
+            "CREATE TABLE [Items] (_id ntext, Symbol ntext);",
+            "INSERT INTO [Items] ([_id], [Symbol]) VALUES ('1', 'hugo')",
+            "INSERT INTO [Items] ([_id], [Symbol]) VALUES ('2', 'torrico')",
+            "INSERT INTO [Items] ([_id], [Symbol]) VALUES ('3', 'marquez')"
+                };
+            foreach (var command in commands)
+            {
+                using (var c = connection.CreateCommand())
+                {
+                    c.CommandText = command;
+                    var rowcount = c.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+    }
+}
